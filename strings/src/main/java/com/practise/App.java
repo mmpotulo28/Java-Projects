c5d35e71909f5da34f4bf5f0350c96ce81b7d936
mmpotulo28 <mpotulom28@gmail.com>
2023-09-12 20:24:12 +0200

    update string project

--- strings/src/main/java/com/practise/App.java
+++ strings/src/main/java/com/practise/App.java
@@ -1,120 +1,121 @@
 package com.practise;
 
+//when doing file operations (java.io)
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
-import java.util.Random;
 import java.io.IOException;
+//the reset
+import java.util.Random;
 
-/**
- * Hello world!
- */
-public final class App {
-    private App() {
-    }
+public final class App { //if the name of your file
+
+    /*
+     * crerating a program to randomly generate sentences
+     * the has to write those sentences on a file
+     * 
+     */
+
+    // global variable declaration
+    static String sentence;
+
+    // static mean that the variable can be accessed anywhere without having to
+    // create an object for it
 
     /**
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        // clear console
+        // clear terminal
         System.out.print("\033[H\033[2J");
 
-        /*** Declare variable ***/
-        String[] article = { "the", "a", "one", "some", "any" };
+        // declare arrays
         String[] noun = { "boy", "girl", "dog", "town", "car" };
+        String[] article = { "the", "one", "a", "some", "any" };
         String[] verb = { "drove", "jumped", "ran", "walked", "skipped" };
         String[] prep = { "to", "from", "over", "under", "on" };
-        // create a string to hold the sentence
-        String sentence = new String();
-        StringBuilder multipleSentence = new StringBuilder();
 
-        // create random number generator
+        /*
+         * randomly pick words on each array
+         * randomly generate i index on each array
+         * we will use the index to pick a certain work on each array
+         */
+
+        // declare random number generator object
         Random random = new Random();
+        StringBuilder multisentences = new StringBuilder();
 
-        for (int i = 0; i < 5; i++) {// for 5 sentences
-            // create random array index for each array (on each array generate a new index)
-            int articleIndex = random.nextInt(article.length);
-            int nounIndex = random.nextInt(noun.length);
-            int verbIndex = random.nextInt(verb.length);
-            int prepIndex = random.nextInt(prep.length);
-            int articleIndex2 = random.nextInt(article.length);
-            int nounIndex2 = random.nextInt(noun.length);
+        // use le object to randomly generate i indices.
+        // index is the position of an element on an array (0-array.length).
+        // we need an index that within bound ze array 0-4.
+        // syntax of the sentences is like, article, noun, verb, prep, article, noun.
 
-            // contatenate each word form the arrray to form a sentence string
-            sentence = article[articleIndex] + " " + noun[nounIndex] + " " + verb[verbIndex] + " " + prep[prepIndex]
-                    + " " + article[articleIndex2] + " " + noun[nounIndex2] + ".";
+        for (int i = 0; i < 6; i++) {
 
-            // Make the first letter of the sentence uppercase (generate a new sentence)
+            int articleIndex = random.nextInt(0, article.length);
+            int nounIndex = random.nextInt(0, noun.length);
+            int verbIndex = random.nextInt(0, verb.length);
+            int prepIndex = random.nextInt(0, prep.length);
+            int articleIndex2 = random.nextInt(0, article.length);
+            int nounIndex2 = random.nextInt(0, article.length);
+
+            // use the indices to get the word(elements) form the arrays\
+            // concate
+            sentence = article[articleIndex] + " " + noun[nounIndex] + " " + verb[verbIndex] + " "
+                    + prep[prepIndex]
+                    + " " + article[articleIndex2] + " " + noun[nounIndex2] + " ";
+
+            // change the 1st letter of the sentence to upperCase
             sentence = sentence.substring(0, 1).toUpperCase() + sentence.substring(1);
 
-            // add each sentence to a multipleSentence string builder (for file writting)
-            multipleSentence.append(sentence + "\n");
-        }
+            // add each sentence into the multiple sentence string builder
+            // appending is like adding/pushing something at the end
+            multisentences.append(sentence + "\n");
+        } // end loop
 
-        // write to file (1st convert back to string, function accepts string)
-        fileOperationWrite(multipleSentence.toString());
-        System.out.println("\n"); // just to create space between the two outputs
+        // extras
+        System.out.println(sentence.charAt(5)); // this wil print the character at 5
+        System.out.println(sentence.indexOf("e")); // this wil print the position of a certain charecter
+        System.out.println(sentence.length()); // this wil print the length of the string
 
-        // read from file
-        fileOperationRead();
-        System.out.println("\n");
-    }
+        // System.out.println(multisentences);
 
-    /************************* File Operations *******************/
+        System.out.println("\n\n");
 
-    public static void fileOperationRead() {
-        // read from file (read the sentence we wrote to the file)
-        try {
-            // create a file reader object
-            FileReader fr = new FileReader("src/main/java/com/practise/sentence.txt");
-            // read the file
-            int i;
-            // (read() returns -1 when there is no more data)
-            while ((i = fr.read()) != -1) { // while there is still data to read from the file
-                // fr.read() returns the character as a number so convert that back to character
-                char c = (char) i;
+        // 2nd part: writting the sentence to a file
+        File myFile = new File("sentences.txt");
 
-                // print character by character
-                System.out.print(c);
-            }
-            // close the file
-            fr.close();
-        } catch (IOException e) {
-            System.out.println("Error reading file");
-        }
-    }
-
-    public static void fileOperationWrite(String sentence) {
-
-        // create a file object
-        File myFile = new File("src/main/java/com/practise/sentence.txt");
-
-        // check if file exists
+        // check if file exits, if not, create a new one.
         if (myFile.exists()) {
-            System.out.println("File already exists");
+            System.out.println("file already exists.");
         } else {
-            try { // saround with try catch block to catch any errors when creating the file
-                  // create a new file
-                myFile.createNewFile();
-                System.out.println("File created successfully" + "\n\n");
-            } catch (Exception e) {
-                System.out.println("Error creating file" + "\n");
+            try { // try creating new file, if it doesnt work catch the error
+                myFile.createNewFile(); // unhandled IO exception
+                System.out.println("New file created!");
+            } catch (IOException e) {
+                System.out.println("Error, couldnt create new file!");
             }
         }
 
-        // write to file
-        try {
-            // create a file writer object
-            FileWriter fw = new FileWriter("src/main/java/com/practise/sentence.txt");
-            // write sentence to file but dont overwrite its content
-            fw.write(sentence);
+        // we can get 2 errrors, access denied/filenotfound
 
-            // close the file
-            fw.close();
-            System.out.println("Successfully wrote to the file.");
+        try { // try writting to this file
+            FileWriter fw = new FileWriter(myFile);
+            // write the sentence to the file
+            fw.write(multisentences.toString());
+            // The method write(int) in the type OutputStreamWriter is not applicable for
+            // the arguments (StringBuilder)
+            fw.close(); // closing line
+
+        } catch (SecurityException exception) {
+            System.out.println("Error! access denied!");
+        } catch (FileNotFoundException e) {
+            System.out.println("Error! file not found");
         } catch (IOException e) {
-            System.out.println("Error writing to file");
+            System.out.println("Couldn't write to file");
         }
+
+        System.out.println("\n\n");
     }
+
 }
